23a5c8dec85a8e670d69486f68eb1a726607ecbe
AnimeFlix007 <devjitbose111@gmail.com>
2023-07-31 12:04:09 +0530

    changes

--- app/components/Common/CaseStudies.js
+++ app/components/Common/CaseStudies.js
@@ -13,10 +13,9 @@ const CaseStudies = () => {
             <div className="casecard">
               <div className="single-case">
                 <div className="case-img">
-                  <a href="/case-studies-details/?title=Pharmeasy%2520Youtube%2520Channel&amp;content=%255B%257B%2522para1%2522%253A%257B%2522text%2522%253A%2522PharmEasy%252C%2520India%27s%2520leading%2520online%2520pharmacy%2520wanted%2520to%2520drive%2520awareness%2520for%2520Type%25201%2520Diabetes%2522%252C%2522src%2522%253A%2522%2522%252C%2522type%2522%253A%2522para%2522%257D%257D%252C%257B%2522para2%2522%253A%257B%2522text%2522%253A%2522%2522%252C%2522src%2522%253A%2522%252Fimages%252Fcases%252Fparmeasy1.png%2522%252C%2522type%2522%253A%2522img%2522%257D%257D%252C%257B%2522para3%2522%253A%257B%2522text%2522%253A%2522%2522%252C%2522src%2522%253A%2522%252Fimages%252Fcases%252Fpharmeasy2.png%2522%252C%2522type%2522%253A%2522img%2522%257D%257D%255D&amp;actualImg=%252Fimages%252Fcase-studies%252Fpharmeasy1.jpg&amp;author=Pratik%2520Gour&amp;date=19%2520April%25202022&amp;tags=Digital%2520Marketing%252CInfluencer%2520Marketing%252CInfluencers%252CBLOG%252CPPC%252CSEO%252CSocial%2520Footprynt%252CFootprynt%252CSocial%2520Media%252CNew%2520Age%2520Marketing&amp;summary=Doctors%2520and%2520wellness%2520experts%2520created%2520videos%2520for%2520the%2520brand%25E2%2580%2599s%2520YouTube%2520channel.%2520Campaign%2520to%2520drive%2520awareness%2520for%2520Type%25201%2520Diabetes%2520through%2520influencer%2520marketing.">
-                    <Image quality={90}
-                      
-                      
+                  <a href="/case-studies-details/2">
+                    <Image
+                      quality={90}
                       loading="lazy"
                       src="/images/case-studies/pharmeasy1.jpg"
                       alt="case"
@@ -27,7 +26,7 @@ const CaseStudies = () => {
                   </a>
                 </div>
                 <div className="content">
-                  <a href="/case-studies-details/?title=Pharmeasy%2520Youtube%2520Channel&amp;content=%255B%257B%2522para1%2522%253A%257B%2522text%2522%253A%2522PharmEasy%252C%2520India%27s%2520leading%2520online%2520pharmacy%2520wanted%2520to%2520drive%2520awareness%2520for%2520Type%25201%2520Diabetes%2522%252C%2522src%2522%253A%2522%2522%252C%2522type%2522%253A%2522para%2522%257D%257D%252C%257B%2522para2%2522%253A%257B%2522text%2522%253A%2522%2522%252C%2522src%2522%253A%2522%252Fimages%252Fcases%252Fparmeasy1.png%2522%252C%2522type%2522%253A%2522img%2522%257D%257D%252C%257B%2522para3%2522%253A%257B%2522text%2522%253A%2522%2522%252C%2522src%2522%253A%2522%252Fimages%252Fcases%252Fpharmeasy2.png%2522%252C%2522type%2522%253A%2522img%2522%257D%257D%255D&amp;actualImg=%252Fimages%252Fcase-studies%252Fpharmeasy1.jpg&amp;author=Pratik%2520Gour&amp;date=19%2520April%25202022&amp;tags=Digital%2520Marketing%252CInfluencer%2520Marketing%252CInfluencers%252CBLOG%252CPPC%252CSEO%252CSocial%2520Footprynt%252CFootprynt%252CSocial%2520Media%252CNew%2520Age%2520Marketing&amp;summary=Doctors%2520and%2520wellness%2520experts%2520created%2520videos%2520for%2520the%2520brand%25E2%2580%2599s%2520YouTube%2520channel.%2520Campaign%2520to%2520drive%2520awareness%2520for%2520Type%25201%2520Diabetes%2520through%2520influencer%2520marketing.">
+                  <a href="/case-studies-details/2">
                     <h3>Pharmeasy Youtube Channel</h3>
                   </a>
                   <p>
@@ -35,10 +34,7 @@ const CaseStudies = () => {
                     YouTube channel. Campaign to drive awareness for Type 1
                     Diabetes through influencer marketing.
                   </p>
-                  <a
-                    className="line-bnt"
-                    href="/case-studies-details/?title=Pharmeasy%2520Youtube%2520Channel&amp;content=%255B%257B%2522para1%2522%253A%257B%2522text%2522%253A%2522PharmEasy%252C%2520India%27s%2520leading%2520online%2520pharmacy%2520wanted%2520to%2520drive%2520awareness%2520for%2520Type%25201%2520Diabetes%2522%252C%2522src%2522%253A%2522%2522%252C%2522type%2522%253A%2522para%2522%257D%257D%252C%257B%2522para2%2522%253A%257B%2522text%2522%253A%2522%2522%252C%2522src%2522%253A%2522%252Fimages%252Fcases%252Fparmeasy1.png%2522%252C%2522type%2522%253A%2522img%2522%257D%257D%252C%257B%2522para3%2522%253A%257B%2522text%2522%253A%2522%2522%252C%2522src%2522%253A%2522%252Fimages%252Fcases%252Fpharmeasy2.png%2522%252C%2522type%2522%253A%2522img%2522%257D%257D%255D&amp;actualImg=%252Fimages%252Fcase-studies%252Fpharmeasy1.jpg&amp;author=Pratik%2520Gour&amp;date=19%2520April%25202022&amp;tags=Digital%2520Marketing%252CInfluencer%2520Marketing%252CInfluencers%252CBLOG%252CPPC%252CSEO%252CSocial%2520Footprynt%252CFootprynt%252CSocial%2520Media%252CNew%2520Age%2520Marketing&amp;summary=Doctors%2520and%2520wellness%2520experts%2520created%2520videos%2520for%2520the%2520brand%25E2%2580%2599s%2520YouTube%2520channel.%2520Campaign%2520to%2520drive%2520awareness%2520for%2520Type%25201%2520Diabetes%2520through%2520influencer%2520marketing."
-                  >
+                  <a className="line-bnt" href="/case-studies-details/2">
                     View Project Details
                   </a>
                 </div>
@@ -49,10 +45,9 @@ const CaseStudies = () => {
             <div className="casecard">
               <div className="single-case">
                 <div className="case-img">
-                  <a href="/case-studies-details/?title=%2520Promoting%2520Download%2520of%2520App%2520through%2520financial%2520influencers&amp;content=%255B%257B%2522para1%2522%253A%257B%2522text%2522%253A%2522Stockal%252Ca%2520platform%2520that%2520allows%2520its%2520users%2520to%2520invest%2520abroad%2520in%2520US%2520markets%252C%2520wanted%2520to%2520drive%2520a%2520customer%2520acquisition%2520and%2520app%2520download%2520campaign%2520thru%2520use%2520of%2520influencers%2522%252C%2522src%2522%253A%2522%2522%252C%2522type%2522%253A%2522para%2522%257D%257D%252C%257B%2522para2%2522%253A%257B%2522text%2522%253A%2522%2522%252C%2522src%2522%253A%2522%252Fimages%252Fcases%252Fstockal1.png%2522%252C%2522type%2522%253A%2522img%2522%257D%257D%252C%257B%2522para3%2522%253A%257B%2522text%2522%253A%2522%2522%252C%2522src%2522%253A%2522%252Fimages%252Fcases%252Fstockal2.png%2522%252C%2522type%2522%253A%2522img%2522%257D%257D%255D&amp;actualImg=%252Fimages%252Fcase-studies%252Fstockal1.jpg&amp;author=Pratik%2520Gour&amp;date=18%2520April%25202022&amp;tags=Digital%2520Marketing%252CInfluencer%2520Marketing%252CInfluencers%252CBLOG%252CPPC%252CSEO%252CSocial%2520Footprynt%252CFootprynt%252CSocial%2520Media%252CNew%2520Age%2520Marketing&amp;summary=KYC%252FLogin%2520entries%2520for%2520Stockal%2520-%2520a%2520Global%2520Investing%2520App%2520-%2520with%2520influencer%2520collaborations.%2520Platform%2520used%2520-%2520Instagram%2520and%2520Youtube.">
-                    <Image quality={90}
-                      
-                      
+                  <a href="/case-studies-details/1">
+                    <Image
+                      quality={90}
                       loading="lazy"
                       src="/images/case-studies/stockal1.jpg"
                       alt="case"
@@ -63,7 +58,7 @@ const CaseStudies = () => {
                   </a>
                 </div>
                 <div className="content">
-                  <a href="/case-studies-details/?title=%2520Promoting%2520Download%2520of%2520App%2520through%2520financial%2520influencers&amp;content=%255B%257B%2522para1%2522%253A%257B%2522text%2522%253A%2522Stockal%252Ca%2520platform%2520that%2520allows%2520its%2520users%2520to%2520invest%2520abroad%2520in%2520US%2520markets%252C%2520wanted%2520to%2520drive%2520a%2520customer%2520acquisition%2520and%2520app%2520download%2520campaign%2520thru%2520use%2520of%2520influencers%2522%252C%2522src%2522%253A%2522%2522%252C%2522type%2522%253A%2522para%2522%257D%257D%252C%257B%2522para2%2522%253A%257B%2522text%2522%253A%2522%2522%252C%2522src%2522%253A%2522%252Fimages%252Fcases%252Fstockal1.png%2522%252C%2522type%2522%253A%2522img%2522%257D%257D%252C%257B%2522para3%2522%253A%257B%2522text%2522%253A%2522%2522%252C%2522src%2522%253A%2522%252Fimages%252Fcases%252Fstockal2.png%2522%252C%2522type%2522%253A%2522img%2522%257D%257D%255D&amp;actualImg=%252Fimages%252Fcase-studies%252Fstockal1.jpg&amp;author=Pratik%2520Gour&amp;date=18%2520April%25202022&amp;tags=Digital%2520Marketing%252CInfluencer%2520Marketing%252CInfluencers%252CBLOG%252CPPC%252CSEO%252CSocial%2520Footprynt%252CFootprynt%252CSocial%2520Media%252CNew%2520Age%2520Marketing&amp;summary=KYC%252FLogin%2520entries%2520for%2520Stockal%2520-%2520a%2520Global%2520Investing%2520App%2520-%2520with%2520influencer%2520collaborations.%2520Platform%2520used%2520-%2520Instagram%2520and%2520Youtube.">
+                  <a href="/case-studies-details/1">
                     <h3>
                       {" "}
                       Promoting Download of App through financial influencers
@@ -74,10 +69,7 @@ const CaseStudies = () => {
                     with influencer collaborations. Platform used - Instagram
                     and Youtube.
                   </p>
-                  <a
-                    className="line-bnt"
-                    href="/case-studies-details/?title=%2520Promoting%2520Download%2520of%2520App%2520through%2520financial%2520influencers&amp;content=%255B%257B%2522para1%2522%253A%257B%2522text%2522%253A%2522Stockal%252Ca%2520platform%2520that%2520allows%2520its%2520users%2520to%2520invest%2520abroad%2520in%2520US%2520markets%252C%2520wanted%2520to%2520drive%2520a%2520customer%2520acquisition%2520and%2520app%2520download%2520campaign%2520thru%2520use%2520of%2520influencers%2522%252C%2522src%2522%253A%2522%2522%252C%2522type%2522%253A%2522para%2522%257D%257D%252C%257B%2522para2%2522%253A%257B%2522text%2522%253A%2522%2522%252C%2522src%2522%253A%2522%252Fimages%252Fcases%252Fstockal1.png%2522%252C%2522type%2522%253A%2522img%2522%257D%257D%252C%257B%2522para3%2522%253A%257B%2522text%2522%253A%2522%2522%252C%2522src%2522%253A%2522%252Fimages%252Fcases%252Fstockal2.png%2522%252C%2522type%2522%253A%2522img%2522%257D%257D%255D&amp;actualImg=%252Fimages%252Fcase-studies%252Fstockal1.jpg&amp;author=Pratik%2520Gour&amp;date=18%2520April%25202022&amp;tags=Digital%2520Marketing%252CInfluencer%2520Marketing%252CInfluencers%252CBLOG%252CPPC%252CSEO%252CSocial%2520Footprynt%252CFootprynt%252CSocial%2520Media%252CNew%2520Age%2520Marketing&amp;summary=KYC%252FLogin%2520entries%2520for%2520Stockal%2520-%2520a%2520Global%2520Investing%2520App%2520-%2520with%2520influencer%2520collaborations.%2520Platform%2520used%2520-%2520Instagram%2520and%2520Youtube."
-                  >
+                  <a className="line-bnt" href="/case-studies-details/1">
                     View Project Details
                   </a>
                 </div>
@@ -88,10 +80,9 @@ const CaseStudies = () => {
             <div className="casecard">
               <div className="single-case">
                 <div className="case-img">
-                  <a href="/case-studies-details/?title=Asian%2520Paints%2520Promoting%2520new%2520age%2520DIY%2520products&amp;content=%255B%257B%2522para1%2522%253A%257B%2522text%2522%253A%2522Launch%2520of%2520DIY%2520products%2520by%2520Asian%2520Paints%2520by%2520collaborating%2520with%2520DIY%2520and%2520home%2520decor%2520experts.%2520Platform%2520used%2520-%2520Youtube.%2522%252C%2522src%2522%253A%2522%2522%252C%2522type%2522%253A%2522para%2522%257D%257D%252C%257B%2522para2%2522%253A%257B%2522text%2522%253A%2522%2522%252C%2522src%2522%253A%2522%252Fimages%252Fcases%252Fasian1.png%2522%252C%2522type%2522%253A%2522img%2522%257D%257D%252C%257B%2522para3%2522%253A%257B%2522text%2522%253A%2522%2522%252C%2522src%2522%253A%2522%252Fimages%252Fcases%252Fasian2.png%2522%252C%2522type%2522%253A%2522img%2522%257D%257D%255D&amp;actualImg=%252Fimages%252Fcase-studies%252Fasianpaints1.jpg&amp;author=Pratik%2520Gour&amp;date=17%2520April%25202022&amp;tags=Digital%2520Marketing%252CInfluencer%2520Marketing%252CInfluencers%252CBLOG%252CPPC%252CSEO%252CSocial%2520Footprynt%252CFootprynt%252CSocial%2520Media%252CNew%2520Age%2520Marketing&amp;summary=Launch%2520of%2520DIY%2520products%2520by%2520Asian%2520Paints%2520by%2520collaborating%2520with%2520DIY%2520and%2520home%2520decor%2520experts.%2520Platform%2520used%2520-%2520Youtube.">
-                    <Image quality={90}
-                      
-                      
+                  <a href="/case-studies-details/0">
+                    <Image
+                      quality={90}
                       loading="lazy"
                       src="/images/case-studies/asianpaints1.jpg"
                       alt="case"
@@ -102,17 +93,14 @@ const CaseStudies = () => {
                   </a>
                 </div>
                 <div className="content">
-                  <a href="/case-studies-details/?title=Asian%2520Paints%2520Promoting%2520new%2520age%2520DIY%2520products&amp;content=%255B%257B%2522para1%2522%253A%257B%2522text%2522%253A%2522Launch%2520of%2520DIY%2520products%2520by%2520Asian%2520Paints%2520by%2520collaborating%2520with%2520DIY%2520and%2520home%2520decor%2520experts.%2520Platform%2520used%2520-%2520Youtube.%2522%252C%2522src%2522%253A%2522%2522%252C%2522type%2522%253A%2522para%2522%257D%257D%252C%257B%2522para2%2522%253A%257B%2522text%2522%253A%2522%2522%252C%2522src%2522%253A%2522%252Fimages%252Fcases%252Fasian1.png%2522%252C%2522type%2522%253A%2522img%2522%257D%257D%252C%257B%2522para3%2522%253A%257B%2522text%2522%253A%2522%2522%252C%2522src%2522%253A%2522%252Fimages%252Fcases%252Fasian2.png%2522%252C%2522type%2522%253A%2522img%2522%257D%257D%255D&amp;actualImg=%252Fimages%252Fcase-studies%252Fasianpaints1.jpg&amp;author=Pratik%2520Gour&amp;date=17%2520April%25202022&amp;tags=Digital%2520Marketing%252CInfluencer%2520Marketing%252CInfluencers%252CBLOG%252CPPC%252CSEO%252CSocial%2520Footprynt%252CFootprynt%252CSocial%2520Media%252CNew%2520Age%2520Marketing&amp;summary=Launch%2520of%2520DIY%2520products%2520by%2520Asian%2520Paints%2520by%2520collaborating%2520with%2520DIY%2520and%2520home%2520decor%2520experts.%2520Platform%2520used%2520-%2520Youtube.">
+                  <a href="/case-studies-details/0">
                     <h3>Asian Paints Promoting new age DIY products</h3>
                   </a>
                   <p>
                     Launch of DIY products by Asian Paints by collaborating with
                     DIY and home decor experts. Platform used - Youtube.
                   </p>
-                  <a
-                    className="line-bnt"
-                    href="/case-studies-details/?title=Asian%2520Paints%2520Promoting%2520new%2520age%2520DIY%2520products&amp;content=%255B%257B%2522para1%2522%253A%257B%2522text%2522%253A%2522Launch%2520of%2520DIY%2520products%2520by%2520Asian%2520Paints%2520by%2520collaborating%2520with%2520DIY%2520and%2520home%2520decor%2520experts.%2520Platform%2520used%2520-%2520Youtube.%2522%252C%2522src%2522%253A%2522%2522%252C%2522type%2522%253A%2522para%2522%257D%257D%252C%257B%2522para2%2522%253A%257B%2522text%2522%253A%2522%2522%252C%2522src%2522%253A%2522%252Fimages%252Fcases%252Fasian1.png%2522%252C%2522type%2522%253A%2522img%2522%257D%257D%252C%257B%2522para3%2522%253A%257B%2522text%2522%253A%2522%2522%252C%2522src%2522%253A%2522%252Fimages%252Fcases%252Fasian2.png%2522%252C%2522type%2522%253A%2522img%2522%257D%257D%255D&amp;actualImg=%252Fimages%252Fcase-studies%252Fasianpaints1.jpg&amp;author=Pratik%2520Gour&amp;date=17%2520April%25202022&amp;tags=Digital%2520Marketing%252CInfluencer%2520Marketing%252CInfluencers%252CBLOG%252CPPC%252CSEO%252CSocial%2520Footprynt%252CFootprynt%252CSocial%2520Media%252CNew%2520Age%2520Marketing&amp;summary=Launch%2520of%2520DIY%2520products%2520by%2520Asian%2520Paints%2520by%2520collaborating%2520with%2520DIY%2520and%2520home%2520decor%2520experts.%2520Platform%2520used%2520-%2520Youtube."
-                  >
+                  <a className="line-bnt" href="/case-studies-details/0">
                     View Project Details
                   </a>
                 </div>
